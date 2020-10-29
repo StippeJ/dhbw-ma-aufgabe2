c9def0698044254ff59453493852575d1071942e
Jan Stippe <janstippe@gmail.com>
2020-10-29 13:42:23 +0100

    feat(MainActivity+SecondActivity): Added new layouts - Updated the strings, colors and styles

--- app/src/main/java/com/example/stippejan/aufgabe2/MainActivity.java
+++ app/src/main/java/com/example/stippejan/aufgabe2/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.stippejan.aufgabe2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_main);
+
+        Button button1 = findViewById(R.id.button_right);
+        button1.setOnClickListener((v) -> {
+            Intent explicitIntent = new Intent(getApplicationContext(), SecondActivity.class);
+            MainActivity.this.startActivity(explicitIntent);
+        });
     }
 }
